b7282514c4984a5adf4cf5127df3c998cea22adc
Aliaksandra Hurskaya <sandrahurskaya@gmail.com>
2023-06-19 15:22:19 +0200

    Forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 import "./slider";
 import modals from "./modules/modals";
 import tabs from "./modules/tabs";
+import forms from "./modules/forms";
 
 window.addEventListener("DOMContentLoaded", () => {
+    "use strict";
+
     modals();
+    forms();
     tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
     tabs(
         ".decoration_slider",
